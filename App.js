8d437af14d967e83da871e677efe7a38de023999
Hays Stanford <stanfordhays@gmail.com>
2018-03-18 14:44:03 -0700

    Linked firebase db credentials. Actual credentials are kept in an unstored file to prevent obvious security vulnerabilities.

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Automatically set to 'production' when published through Expo
+var env = process.env.NODE_ENV || 'development';
+// var env = 'production'
+var config = require('./config')[env];
+
+const firebaseConfig = {
+   apiKey: config.database.firebaseApiKey,
+   authDomain: config.database.firebaseAuthDomain,
+   databaseURL: config.database.databaseURL,
+ } 
+
+firebase.initializeApp(firebaseConfig)
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={{fontSize: 50,}}>Safex</Text>
+        <Text style={{fontSize: 20, textAlign: 'center'}}>Verify Sexual Consent Before Anything Happens.</Text>
       </View>
     );
   }
@@ -14,7 +28,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b9afff',
     alignItems: 'center',
     justifyContent: 'center',
   },
