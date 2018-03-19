57e6cc5d1e0534fba39c70d519ee1e8abd84b4b1
Hays Stanford <stanfordhays@gmail.com>
2018-03-19 14:16:05 -0700

    Added textInputs for email and password on home screen.

--- screens/Home.js
+++ screens/Home.js
@@ -7,14 +7,42 @@ import {
   View,
   Dimensions,
   InteractionManager,
+  TextInput,
 } from 'react-native';
 
+const {height, width} = Dimensions.get('window');
+
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={{fontSize: 50,}}>Safex</Text>
         <Text style={{fontSize: 20, textAlign: 'center'}}>Verify Sexual Consent Before Anything Happens.</Text>
+        <View style={{flex: 1}}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Email"
+            blurOnSubmit={true}
+            onChangeText={(text) => this.setState({email: text})}
+            value={this.state.email}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            blurOnSubmit={true}
+            onChangeText={(text) => this.setState({password: text})}
+            value={this.state.password}
+          />
+        </View>
       </View>
     );
   }
@@ -26,5 +54,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#b9afff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    flex: 1,
+    width: width,
+    height: 38,
+    lineHeight: 28,
+    alignSelf: 'flex-start',
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontSize:26,
+    color: '#565656',
+    textAlign: 'left',
+    backgroundColor: 'white',
   },
 });
